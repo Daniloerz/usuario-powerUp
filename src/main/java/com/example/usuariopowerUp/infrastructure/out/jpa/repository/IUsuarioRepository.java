4d49e36ea3018624617bbb2200e44fa235d73d70
Danilo Ramirez <danilo0613@hotmail.com>
2024-04-13 09:17:36 -0500

    Git inicial para food court app

--- src/main/java/com/example/usuariopowerUp/infrastructure/out/jpa/repository/IUsuarioRepository.java
+++ src/main/java/com/example/usuariopowerUp/infrastructure/out/jpa/repository/IUsuarioRepository.java
@@ -3,6 +3,9 @@ package com.example.usuariopowerUp.infrastructure.out.jpa.repository;
 import com.example.usuariopowerUp.infrastructure.out.jpa.entity.UsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IUsuarioRepository extends JpaRepository<UsuarioEntity, Integer> {
 
+    Optional<UsuarioEntity> findOneByCorreo (String correo);
 }
